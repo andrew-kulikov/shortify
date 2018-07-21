ea1773c7fa415081d2bab3e88beb30e6d28db374
Andrew <andrew.1.kulikov@gmail.com>
2018-07-21 21:49:54 +0300

    Create Shortcut model

--- shortify/shortcutter/models.py
+++ shortify/shortcutter/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Shortcut(models.Model):
+    long_link = models.CharField(max_length=100, default='', blank=True, null=True)
+    short_link = models.CharField(max_length=100, blank=True, null=True, default='')
