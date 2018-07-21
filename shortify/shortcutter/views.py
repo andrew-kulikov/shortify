3f9e2b366f84eb2fcbc525fef8127d89a3a79101
Andrew <andrew.1.kulikov@gmail.com>
2018-07-21 21:26:56 +0300

    Add base view

--- shortify/shortcutter/views.py
+++ shortify/shortcutter/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 
 
 def index(request):
-    return render(request, 'base.html')
+    context = {}
+
+    if request.method == 'GET':
+        if 'url' in request.GET:
+            url = request.GET['url']
+            url = request.META['HTTP_HOST'] + '/ok228'
+            messages.info(request, 'Your short url: {}'.format(url), extra_tags='alert-success')
+
+    return render(request, 'base.html', context)
 
 
 def short_url(request):
